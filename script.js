df3b566bc980bedf22032c1f704d65942639bd97
YoonGyu-sun <YoonGyu-sun>
2024-02-13 00:48:09 +0900

    noonaPr two

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let playButton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
 let resetButton = document.getElementById("reset-button");
-let chanses = 5;
+let chances = 5;
 let gameOver = false;
 let chanceArea = document.getElementById("chance-area");
-let history = []
+let history = [];
 
 playButton.addEventListener("click", play);
 resetButton.addEventListener("click", reset);
@@ -15,9 +15,12 @@ userInput.addEventListener("focus", function(){userInput.value=""});
 
 function pickRandoNum(){
     computerNum = Math.floor(Math.random() * 100) + 1;
+    // console.log(computerNum);
 }
 
+
 function play(){
+    
     let userValue = userInput.value;
 
     if(userValue<1 || userValue>100){
@@ -25,25 +28,27 @@ function play(){
         return;
     }
 
-    if(history == userValue){
-        resultArea.textContent="이미 입력했던 숫자입니다. 다른 숫자를 입력하세요.";
+    if(history.includes(userValue)){
+        resultArea.textContent="이미 입력되었던 숫자입니다.";
         return;
     }
-        chanses --;
-        chanceArea.textContent = `남은 기회 : ${chanses}번`;
+        chances --;
+        // console.log(chances);
+        chanceArea.textContent = `남은 기회 : ${chances}번`;
 
         if(userValue < computerNum){
-            resultArea.textContent="UP";
+            resultArea.textContent="이 숫자보다 더 큰 숫자입니다.";
         }else if (userValue > computerNum){
-            resultArea.textContent="Down";
+            resultArea.textContent="이 숫자보다 더 작은 숫자입니다.";
         }else{
             resultArea.textContent="정답";
             gameOver = true;
         }
 
         history.push(userValue);
+        
 
-        if(chanses < 1){
+        if(chances < 1){
             gameOver = true;
         }
         if(gameOver == true){
@@ -56,8 +61,10 @@ function reset(){
     userInput.value = "";
     pickRandoNum();
     playButton.disabled = false;
-    resultArea.textContent = "결과값이 여기 나옵니다";
-    // chanses = 5;
+    history = [];
+    // console.log(history);
+    resultArea.textContent = "새로운 숫자로 변경되었습니다.";
+    chances =5;
     chanceArea.textContent = `남은 기회 : 5번`;
 }
 
